f9535ad69af24dc035024663adc212e57b18dea0
Alan <alan@hypecode.com>
2020-05-13 16:20:51 +0200

    refacto SignIn SignUp

--- views/signInView.tsx
+++ views/signInView.tsx
@@ -6,110 +6,124 @@ import { State } from 'react-native-gesture-handler';
 import * as Helpers from '../helpers';
 
 interface Props {
-  navigation: any
+    navigation: any
 }
 
 export default class SignIn extends Component<Props> {
 
-  state = {
-    email: 'julien@hypecode.com',
-    password: 'password',
-    isLoading: false
-  }
-
-  static navigationOptions = ({ navigation }) => {
-    return {
-      headerShown: false
+    state = {
+        email: 'julien@hypecode.com',
+        password: 'password',
+        isLoading: false
     }
-  }
 
+    static navigationOptions = ({ navigation }) => {
+        return {
+            headerShown: false
+        }
+    }
 
-  login = () => {
-    this.setState({ isLoading: true });
-    const payload = {
-      email: this.state.email,
-      password: this.state.password,
-    };
+    login = () => {
+        this.setState({ isLoading: true });
+        const payload = {
+            email: this.state.email,
+            password: this.state.password,
+        };
 
-    Helpers.requestService('login', 'POST', payload).then((res: any) => {
-      if (res.token) {
-        alert('Sucess ! you will be logged in !');
-        Helpers.storeDataLocally('userAccountToken', res.token).catch((err) => console.log(err));
-        this.props.navigation.navigate('LogIn');
-      }
-      else {
-        alert('Something went wrong...')
-      }
-    });
-  }
+        // Call API
+        Helpers.requestService('login', 'POST', payload).then((res: any) => {
+            // if token retrieve
+            if (res.token) {
+                alert('Sucess ! You\'re logged in !');
+                // token saved in locally
+                Helpers.storeDataLocally('token', res.token).catch((err) => console.log(err));
+                // redirect to ->
+                this.props.navigation.navigate('LogIn');
+            }
+            else {
+                alert('Something went wrong...')
+            }
+        });
+    }
 
-  componentWillMount() {
-    Helpers.getDataLocally('userAccountToken').then((res) => {
-      // if(tokenisvalid) {
-      //   this.props.navigation.navigate('LogIn');
-      // }
-      console.log('response from local store : ', res);
-    }).catch(err => console.log(err));
-  }
+    componentWillMount() {
+        Helpers.getDataLocally('-token').then((res) => {
+            console.log('local store : ', res);
+            if (res) {
+                this.props.navigation.navigate('LogIn');
+            }
+        }).catch(err => {
+            console.log(err)
+            // Handle the error case.
+        });
+    }
 
-  render() {
-    const isLoading = this.state.isLoading;
+    render() {
+        const isLoading = this.state.isLoading;
 
-    return (
-      <View style={styles.container}>
-        <Image source={require('../assets/landingBackground.jpg')} style={styles.backgroundImage}
-          resizeMode='cover' blurRadius={4} />
-
-        <LinearGradient colors={['#D42D4EAA', '#B11231AA', '#8D011DAA']}
-          style={{ height: '100%', width: '100%', alignItems: 'center', justifyContent: 'space-around' }}>
-
-          <Text style={{ position: 'absolute', fontFamily: 'NotoSans-Bold', color: 'white', fontSize: 20, top: '10%' }}>There's always time to Catch
-            up!</Text>
-
-          <View style={{ width: '80%', justifyContent: 'center', alignItems: 'center' }}>
-            <View style={{ width: '100%', height: 200, justifyContent: 'space-between' }}>
-              {isLoading ? (<ActivityIndicator />) : (
-                <React.Fragment>
-                  <View style={{ margin: 10 }}>
-                    <Input inputStyle={{ color: 'white' }} placeholder='E-mail or pseudo' onChangeText={(email) => this.setState({ email })} />
-                    <Input inputStyle={{ color: 'white' }} placeholder='Password' onChangeText={(password) => this.setState({ password })} />
-                  </View>
-                  <View>
-                    <Button title="Login" containerStyle={{ padding: 5 }} titleStyle={{ color: '#eeeeee' }}
-                      buttonStyle={{ backgroundColor: '#8D011D' }}
-                      onPress={() => this.login()}
-                    />
-                    <Button title="Get started" containerStyle={{ padding: 5 }} titleStyle={{ color: '#eeeeee' }}
-                      buttonStyle={{ backgroundColor: '#8D011D' }}
-                      onPress={() => this.props.navigation.navigate('SignUp')}
-                    />
-                  </View>
-                </React.Fragment>
-              )}
-              <Text style={{ alignSelf: 'flex-end', padding: 5, color: '#eeeeee' }}>Can't acess your account ?</Text>
+        return (
+            <View style={styles.container}>
+                <Image source={require('../assets/landingBackground.jpg')} style={styles.backgroundImage} resizeMode='cover' blurRadius={4} />
+                <LinearGradient colors={['#D42D4EAA', '#B11231AA', '#8D011DAA']} style={styles.linearGradient}>
+                    <Text style={styles.title}>LOVE ON</Text>
+                    {/* TODO : ADD LOGO HERE */}
+                    <View style={styles.form}>
+                        {isLoading ? (<ActivityIndicator />) : (
+                            <React.Fragment>
+                                <View style={{ margin: 10 }}>
+                                    <Input inputStyle={{ color: 'white' }} placeholderTextColor="#FFF" placeholder='E-mail' onChangeText={(email) => this.setState({ email })} />
+                                    <Input inputStyle={{ color: 'white' }} placeholderTextColor="#FFF" placeholder='Password' onChangeText={(password) => this.setState({ password })} />
+                                </View>
+                                <View>
+                                    <Button title="Login" containerStyle={{ padding: 5 }} titleStyle={{ color: '#eeeeee' }}
+                                        buttonStyle={{ backgroundColor: '#8D011D' }}
+                                        onPress={() => this.login()}
+                                    />
+                                    <Button title="Get started" containerStyle={{ padding: 5 }} titleStyle={{ color: '#eeeeee' }}
+                                        buttonStyle={{ backgroundColor: '#8D011D' }}
+                                        onPress={() => this.props.navigation.navigate('SignUp')}
+                                    />
+                                </View>
+                            </React.Fragment>
+                        )}
+                        {/* <Text style={{ alignSelf: 'flex-end', padding: 5, color: '#eeeeee' }}>Can't acess your account ?</Text> */}
+                    </View>
+                </LinearGradient>
             </View>
-          </View>
-
-        </LinearGradient>
-
-      </View>
-    );
-  }
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  backgroundImage: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%'
-  },
-  label: {
-    color: 'white',
-  }
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    backgroundImage: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%'
+    },
+    form: {
+        width: '80%',
+        height: 200,
+        justifyContent: 'space-between'
+    },
+    label: {
+        color: 'white',
+    },
+    title: {
+        position: 'absolute',
+        fontFamily: 'NotoSans-Bold',
+        color: 'white',
+        fontSize: 20,
+        top: '10%'
+    },
+    linearGradient: {
+        height: '100%',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'space-around'
+    }
 });
