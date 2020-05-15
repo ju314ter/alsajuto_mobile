9949f323e73541c4972cd3d054109469772c0cf2
Alan <alan@hypecode.com>
2020-05-15 13:15:12 +0200

    update constant

--- Utils/constant.js
+++ Utils/constant.js
@@ -1,5 +1,8 @@
-export const API_URL = process.env.API_URL
+export const API_URL = process.env.API_URL || 'https://alsatoju-dev.herokuapp.com'
 
 export const TOKEN = 'token'
 export const LOGIN = `${API_URL}/login`
-export const MATCH = `${API_URL}/matchings`
+export const MATCHS = `${API_URL}/matchings`
+export const REFRESH = `${API_URL}/matchings/refresh`
+export const GAMES = `${API_URL}/games`
+export const USERS = `${API_URL}/app_users`
