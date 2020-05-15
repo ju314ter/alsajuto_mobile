8cbf4fb34a14cac8290595dca151cca093260ef2
Alan <alan@hypecode.com>
2020-05-15 17:00:04 +0200

    standard JS

--- views/matching/matchListView.tsx
+++ views/matching/matchListView.tsx
@@ -1,43 +1,42 @@
-import React, { useState, useEffect, useReducer } from 'react';
-import { StyleSheet, Text, TextInput, View, ScrollView, ActivityIndicator } from 'react-native';
-import * as Helpers from '../../helpers';
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, View, ActivityIndicator } from 'react-native'
+import * as Helpers from '../../helpers'
 
+import Match from '../../components/Match'
 
-import Match from '../../components/Match';
+export default function MatchListView ({ navigation }) {
+  const [isLoading, setLoading] = useState(false)
+  const [matchList, setMatchList] = useState([])
 
-export default function MatchListView({ navigation }) {
-    const [isLoading, setLoading] = useState(false);
-    const [matchList, setMatchList] = useState([]);
+  useEffect(() => {
+    Helpers.getDataLocally('token').then((token) => {
+      // Fetch Match list here
+      if (token) {
+        // Helpers.requestService('GET', 'matchings').then((res) => {
+        //     console.log(res)
+        // })
+      }
+    })
+  }, [])
 
-    useEffect(() => {
-        Helpers.getDataLocally('token').then((token) => {
-            // Fetch Match list here
-            if (token) {
-                // Helpers.requestService('GET', 'matchings').then((res) => {
-                //     console.log(res)
-                // })
-            }
-        })
-    }, []);
-
-    if (!isLoading) {
-        return (
-            <View style={styles.container}>
-                <React.Fragment>
-                    <Match navigation={navigation} name='Catherine'></Match>
-                    <Match navigation={navigation} name='Jennyfer'></Match>
-                </React.Fragment>
-            </View>
-        )
-    } else {
-        return <ActivityIndicator />
-    }
+  if (!isLoading) {
+    return (
+      <View style={styles.container}>
+        <>
+          <Match navigation={navigation} name='Catherine'>.</Match>
+          <Match navigation={navigation} name='Jennyfer'>.</Match>
+        </>
+      </View>
+    )
+  } else {
+    return <ActivityIndicator />
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'white',
-        width: '100%',
-        height: '100%'
-    }
+  container: {
+    backgroundColor: 'white',
+    width: '100%',
+    height: '100%'
+  }
 })
