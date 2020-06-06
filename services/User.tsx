9cc1167ff75fe464a864a7cec57277309c5db07f
Alan <alan@hypecode.com>
2020-06-06 19:16:48 +0200

    clean service user

--- services/User.tsx
+++ services/User.tsx
@@ -1,18 +1,15 @@
 import axios from 'axios'
 import * as constant from '../Utils/constant.js'
 import { handler } from './handler'
-import { setAuthorization } from '../services/provider'
 
 export async function login (data) {
-  console.log(constant.LOGIN)
   try {
+    if (axios.defaults.headers.common["Authorization"]) { delete axios.defaults.headers.common["Authorization"] }
     const res = await axios.post(constant.LOGIN, data)
-    console.log('### LOGIN ###')
-    console.log(res.data)
-    console.log('### END LOGIN ###')
+    axios.defaults.headers.common["Authorization"] = `Bearer ${res.data.token}`
     return res.data
   } catch (e) {
-    console.log('Catch login(): ' + e)
+    console.log('Catch login() :' + e)
     throw handler(e)
   }
 }
@@ -20,8 +17,6 @@ export async function login (data) {
 export async function patch (param, data) {
   try {
     const res = await axios.patch(constant.USERS + '/' + param, data)
-    console.log('Patch :')
-    console.log(res.data)
     return res.data
   } catch (e) {
     console.log('Catch Patch User: ' + e)
@@ -29,17 +24,25 @@ export async function patch (param, data) {
   }
 }
 
-export async function getMe(token = false) {
+export async function getMe() {
   try {
-    if (token) {
-      setAuthorization(token)
-    }
     const res = await axios.get(constant.MY_PROFIL)
     console.log('getMe :')
     console.log(res.data)
     return res.data
   } catch (e) {
     console.log('Catch getMe() User: ' + e)
+    throw handler(e)
+  }
+}
+
+export async function getMyProfilPicture() {
+  try {
+    const res = await axios.get(constant.PROFIL_PICTURE)
+    if (res.data) return res.data
+    else return null
+  } catch (e) {
+    console.log('catch GetMyProfilPicture : ', e)
     throw handler(e)
   }
 }
